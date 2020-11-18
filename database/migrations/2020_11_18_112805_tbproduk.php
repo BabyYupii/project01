737963edd7d3e6acf07137181c4fb777fdc8b74b
Valdo Dicky <faldo.diki22@gmail.com>
2020-11-18 19:36:36 +0800

    nyoba ulang

---
+++ database/migrations/2020_11_18_112805_tbproduk.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tbproduk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //bagian restore data
+        schema::create('produks',function(Blueprint $tb){
+            $tb->increments('ID');
+            $tb->string('Nama',20);
+            $tb->integer('ID_KATEGORI');
+            $tb->float('HARGA_BELI');
+            $tb->float('HARGA_JUAL');
+            $tb->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //bagian drop data
+        schema::dropIfExist('produks');
+    }
+}
