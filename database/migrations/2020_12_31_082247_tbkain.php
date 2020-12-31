5212611c4800792b90c555c418b5d0b8d048bd82
Valdo Dicky <faldo.diki22@gmail.com>
2020-12-31 20:51:54 +0800

    prak 13 blm fix

---
+++ database/migrations/2020_12_31_082247_tbkain.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tbkain extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 
+        schema::create('Kain',function(Blueprint $tb){
+            $tb->increments('ID');
+            $tb->string('Nama_Kain',100);
+            $tb->varchar('Kategorii',100);
+            $tb->integer('qty');
+            $tb->varchar('Distributor',100);
+            $tb->float('Harga_Jual');
+            $tb->float('Harga_Beli');
+            $tb->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_12_31_084217_tbkategorii.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Tbkategorii extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        schema::create('kategori', function(Blueprint $tb) {
+            $tb->id();
+            $tb->string('Kategori',200);
+            $tb->string('Keterangan',255);
+            $tb->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
